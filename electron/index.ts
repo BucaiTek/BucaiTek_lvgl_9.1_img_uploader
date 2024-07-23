9acce8eeec8e3a6211da9a720d24b5fa4875a898
GG <zjjhgzh@gmail.com>
2024-07-24 02:27:03 +0800

    work

--- electron/index.ts
+++ electron/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron'
 import { spawn } from 'child_process'
+
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -8,67 +9,101 @@ function createWindow() {
     minHeight: 600
   })
 
-  mainWindow.webContents.session.on('select-hid-device', (event, details, callback) => {
-    //Add events to handle devices being added or removed before the callback on
-    //`select-hid-device` is called.
-    mainWindow.webContents.session.on('hid-device-added', (event, device) => {
-      console.log('hid-device-added FIRED WITH', device)
-      //Optionally update details.deviceList
-    })
-
-    mainWindow.webContents.session.on('hid-device-removed', (event, device) => {
-      console.log('hid-device-removed FIRED WITH', device)
-      //Optionally update details.deviceList
-    })
-
-    event.preventDefault()
-    if (details.deviceList && details.deviceList.length > 0) {
-      callback(details.deviceList[0].deviceId)
-    }
-  })
-
-  mainWindow.webContents.session.setPermissionCheckHandler(
-    (webContents, permission, requestingOrigin, details) => {
-      if (permission === 'hid' && details.securityOrigin === 'file:///') {
-        return true
-      }
-    }
-  )
-
-  mainWindow.webContents.session.setDevicePermissionHandler((details) => {
-    if (details.deviceType === 'hid' && details.origin === 'file://') {
-      return true
-    }
-  })
-
   mainWindow.loadFile('dist/index.html')
-  // 启动子进程来执行 Bash 命令获取 sensor 型号
-  const child = spawn('./smc', ['list'])
-  let sensorInfo = ''
 
+  let cpuModel = ''
+
+  // 启动子进程来执行 Bash 命令获取 CPU 型号
+  const child = spawn('sysctl', ['-a'])
   child.stdout.on('data', (data) => {
-    sensorInfo += data.toString()
+    cpuModel += data.toString()
   })
 
-  child.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`)
+  child.on('close', () => {
+    const match = cpuModel.match(/machdep.cpu.brand_string:\s(.+)/)
+    if (match) {
+      cpuModel = match[1]
+      console.log('CPU 型号:', cpuModel)
+      // 读取传感器数据
+      fetchSensorData(cpuModel)
+    }
   })
 
-  child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`)
-    console.log(sensorInfo)
-    // 可以选择发送信息到渲染进程，或做其他处理
-    mainWindow.webContents.send('sensor-model', sensorInfo)
-  })
+  // 根据 CPU 型号获取传感器数据
+  function fetchSensorData(cpuModel) {
+    let efficiencyCores = [] as string[]
+    let performanceCores = [] as string[]
+    let gpuCores = [] as string[]
+
+    if (cpuModel.includes('M1')) {
+      efficiencyCores = ['Tp09', 'Tp0T']
+      performanceCores = ['Tp01', 'Tp05', 'Tp0D', 'Tp0H', 'Tp0L', 'Tp0P', 'Tp0X', 'Tp0b']
+      gpuCores = ['Tg05', 'Tg0D', 'Tg0L', 'Tg0T']
+    } else if (cpuModel.includes('M2')) {
+      efficiencyCores = ['Tp1h', 'Tp1t', 'Tp1p', 'Tp1l']
+      performanceCores = ['Tp01', 'Tp05', 'Tp09', 'Tp0D', 'Tp0X', 'Tp0b', 'Tp0f', 'Tp0j']
+      gpuCores = ['Tg0f', 'Tg0j']
+    } else if (cpuModel.includes('M3')) {
+      efficiencyCores = ['Te05', 'Te0L', 'Te0P', 'Te0S']
+      performanceCores = [
+        'Tf04',
+        'Tf09',
+        'Tf0A',
+        'Tf0B',
+        'Tf0D',
+        'Tf0E',
+        'Tf44',
+        'Tf49',
+        'Tf4A',
+        'Tf4B',
+        'Tf4D',
+        'Tf4E'
+      ]
+      gpuCores = ['Tf14', 'Tf18', 'Tf19', 'Tf1A', 'Tf24', 'Tf28', 'Tf29', 'Tf2A']
+    }
+
+    const allSensors = efficiencyCores.concat(performanceCores, gpuCores)
+    const child = spawn('./sys_reader', ['list'])
+    let sensorInfo = ''
+
+    child.stdout.on('data', (data) => {
+      sensorInfo += data.toString()
+    })
+
+    child.on('close', () => {
+      processSensorData(sensorInfo, allSensors)
+    })
+  }
+
+  // 处理传感器数据，过滤并输出特定的传感器值
+  function processSensorData(sensorData, sensorList) {
+    const lines = sensorData.split('\n')
+    const sensorValues = {}
+
+    lines.forEach((line) => {
+      const parts = line.split(/\s+/)
+      if (parts.length === 2) {
+        const key = parts[0].replace(/[^\w]/g, '')
+        if (sensorList.includes(key)) {
+          const value = parseFloat(parts[1])
+          sensorValues[key] = value
+          console.log(`${key}: ${value}`)
+        }
+      }
+    })
+  }
+
+  // 定时调用 fetchSensorData 函数
+  setInterval(() => fetchSensorData(cpuModel), 2000)
 }
 
 app.whenReady().then(() => {
   createWindow()
-  app.on('activate', function () {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
