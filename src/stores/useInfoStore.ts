27df4cfdd7942afa871f1c4b1ff3d6650f577480
BucaiTek <bucaitechnology@gmail.com>
2024-08-04 13:11:26 +0800

    update weather code for local language

--- src/stores/useInfoStore.ts
+++ src/stores/useInfoStore.ts
@@ -50,16 +50,32 @@ export const useInfoStore = defineStore('infoStore', {
     },
     async fetchWeatherInfo() {
       try {
-        let json = (
-          await axios.get(
-            'https://64090833871b4162bbbdbe9d92b87a1a-cn-shenzhen.alicloudapi.com/weather.json?q=' +
-              this.city
-          )
-        ).data
-        this.weather = json['current']['condition']['code']
-        this.temperature.current = json['current']['temp_c']
-        this.temperature.min = json['forecast']['forecastday'][0]['day']['mintemp_c']
-        this.temperature.max = json['forecast']['forecastday'][0]['day']['maxtemp_c']
+        let weatherResponse = await axios.get(
+          'https://64090833871b4162bbbdbe9d92b87a1a-cn-shenzhen.alicloudapi.com/weather.json?q=' +
+            this.city
+        )
+        let currentWeather = weatherResponse.data.current.condition.code
+
+        let conditionsResponse = await axios.get('https://www.weatherapi.com/docs/conditions.json')
+        let conditions = conditionsResponse.data.find((c: any) => c.code === currentWeather)
+
+        // Determine the system language
+        const systemLanguage = navigator.language.split('-')[0]
+        console.log('System language:', systemLanguage)
+
+        // Find the language-specific description for the current condition
+        let languageDescription = conditions.languages.find(
+          (lang: any) => lang.lang_iso === systemLanguage
+        )
+        if (languageDescription) {
+          this.weather = `${languageDescription.day_text} / ${languageDescription.night_text}`
+        } else {
+          console.log('No description available for your language.')
+        }
+
+        this.temperature.current = weatherResponse.data.current.temp_c
+        this.temperature.min = weatherResponse.data.forecast.forecastday[0].day.mintemp_c
+        this.temperature.max = weatherResponse.data.forecast.forecastday[0].day.maxtemp_c
       } catch (error) {
         console.error('Failed to fetch weather info:', error)
       }
